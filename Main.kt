e7b2e89da64830277d434f5e61878e9f837be913
Najla Abothnain <60915559+Nsfabo2@users.noreply.github.com>
2021-09-19 12:15:41 +0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    print(" Please enter your name:")
+    val name = readLine()
+    println("Hello $name welcome to my program! ")
+    //Bonus:
+    println(" Please enter your name:")
+    var user_name = readLine()
+    val msg = "Hello $user_name,welcome to my program!"
+    println(msg)
+
+}
